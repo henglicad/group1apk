870a1eb15f0033475e7e769d1d4c1757787d4940
kbef <kaleabsolo1@gmail.com>
2020-01-28 20:13:10 -0800

    slight changes

--- app/src/main/java/com/group1/EngPlan/MainActivity.java
+++ app/src/main/java/com/group1/EngPlan/MainActivity.java
@@ -1,8 +1,11 @@
 package com.group1.EngPlan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mainActivitybtn2 = (Button) findViewById(R.id.MainActivitybtn2);
+        mainActivitybtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent quickView = new Intent(getApplicationContext(), QuickView.class);
+                startActivity(quickView);
+            }
+        });
+
+        //@Override
+        //public void onClick(View v){
+          //  Intent quickView = new Intent(this, QuickView.class);
+            //startActivity(quickView);
+        //}
+
     }
 }
