9b48b6324b01ae9ad8b5ea04bc2e083e066458ab
kbef <kaleabsolo1@gmail.com>
2020-02-01 12:42:54 -0800

    slight changes

--- app/src/main/java/com/group1/EngPlan/MainActivity.java
+++ app/src/main/java/com/group1/EngPlan/MainActivity.java
@@ -9,12 +9,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseHandler myDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button mainActivitybtn2 = (Button) findViewById(R.id.MainActivitybtn2);
+
+        myDB = new DatabaseHandler(this);
+
         mainActivitybtn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/group1/EngPlan/QuickView.java
+++ app/src/main/java/com/group1/EngPlan/QuickView.java
@@ -1,30 +1,53 @@
 package com.group1.EngPlan;
 
+import android.database.Cursor;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import java.util.ArrayList;
 
 public class QuickView extends AppCompatActivity {
+
+    DatabaseHandler myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quick_view);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        ListView listView = (ListView) findViewById(R.id.ListViewIdeal);
+        myDB = new DatabaseHandler(this);
+
+        ArrayList<String> idealSchedule = new ArrayList<>();
+        Cursor data = myDB.returnValue();
+
+        if(data.getCount == 0){
+            Toast.makeText(QuickView.this, "The Database Table is Empty. ",Toast.LENGTH_LONG).show();
+        }
+        else{
+            while(data.moveToNext()){
+                idealSchedule.add(data.getString(0));
+                ListAdapter listAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, idealSchedule);
+                listView.setAdapter(listAdapter);
+            }
+        }
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+
     }
 }
