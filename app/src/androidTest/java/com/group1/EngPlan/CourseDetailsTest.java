c2a671228219dd72bc78f42b04f36665e04d6338
L-Guidos <44513318+L-Guidos@users.noreply.github.com>
2020-03-27 15:30:32 -0700

    Added updated course details test

--- app/src/androidTest/java/com/group1/EngPlan/CourseDetailsTest.java
+++ app/src/androidTest/java/com/group1/EngPlan/CourseDetailsTest.java
@@ -1,7 +1,6 @@
 package com.group1.EngPlan;
 
 
-import android.database.Cursor;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -12,18 +11,13 @@ import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
-import com.group1.EngPlan.Backend.DatabaseHandler;
-
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.junit.Assert.assertEquals;
 
-import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.pressBack;
@@ -40,18 +34,12 @@ import static org.hamcrest.Matchers.is;
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class CourseDetailsTest {
-    DatabaseHandler dbh;
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    @Before
-    public void setUp(){
-        dbh = new DatabaseHandler(getApplicationContext());
-    }
-
     @Test
-    public void courseDetailsTest() {
+    public void cD3() {
         ViewInteraction appCompatButton = onView(
                 allOf(withId(R.id.MainActivitybtn2), withText("Quick View"),
                         childAtPosition(
@@ -71,66 +59,38 @@ public class CourseDetailsTest {
         linearLayout.perform(click());
 
         ViewInteraction textView = onView(
-                allOf(withId(R.id.courseInfoTextView), withText("EPHY1150"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
+                allOf(withId(R.id.courseInfoTextView), withText("EPHY1150")));
         textView.check(matches(withText("EPHY1150")));
 
         ViewInteraction textView2 = onView(
-                allOf(withId(R.id.CourseName), withText("Physics for Engineers 1"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
+                allOf(withId(R.id.CourseName), withText("Physics for Engineers 1")));
         textView2.check(matches(withText("Physics for Engineers 1")));
 
-        // check database is made with correct values
-        Cursor data = dbh.getCourseData("EPHY1150");
-        data.moveToFirst();
-
-        String temp = data.getString(1);
-        assertEquals("Physics for Engineers 1", temp);
-
         ViewInteraction textView3 = onView(
-                allOf(withId(R.id.Offering), withText("This course is offered in Fall semester"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
+                allOf(withId(R.id.Offering), withText("This course is offered in Fall semester")));
         textView3.check(matches(withText("This course is offered in Fall semester")));
 
-        // check database is made with correct values
-        temp = data.getString(2);
-        assertEquals("F", temp);
-
         ViewInteraction textView4 = onView(
-                allOf(withId(R.id.ToList), withText("EPHY1250\nEPHY1700\nEPHY1990\n"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                4),
-                        isDisplayed()));
-        textView4.check(matches(withText("EPHY1250\nEPHY1700\nEPHY1990\n")));
+                allOf(withId(R.id.ToListTitle), withText("Postrequisites")));
+        textView4.check(matches(withText("Postrequisites")));
 
-        // check database is made with correct values (no prereqs)
-        temp = data.getString(3);
-        assertEquals(null, temp);
-        temp = data.getString(4);
-        assertEquals(null, temp);
+        ViewInteraction textView5 = onView(
+                allOf(withId(R.id.ToList), withText("EPHY1250")));
+        textView5.check(matches(withText("EPHY1250")));
 
-        // check database is made with correct values (to list)
-        temp = data.getString(5);
-        temp = temp + " " + data.getString(6) + " " + data.getString(7) + " ";
-        assertEquals("EPHY1250 EPHY1700 EPHY1990 ", temp);
+        ViewInteraction textView6 = onView(
+                allOf(withId(R.id.ToList2), withText("EPHY1700")));
+        textView6.check(matches(withText("EPHY1700")));
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(R.id.ToList), withText("EPHY1250")));
+        appCompatTextView.perform(click());
+
+        ViewInteraction textView7 = onView(
+                allOf(withId(R.id.courseInfoTextView), withText("EPHY1250")));
+        textView7.check(matches(withText("EPHY1250")));
+
+        pressBack();
 
         pressBack();
     }
